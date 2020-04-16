30f91f5cea6695cbec5a7e755b0ffc86c5952fc7
Jordan <jojohersh@gmail.com>
2020-04-16 12:45:56 -0400

    can now successfully read an image and draw it to the canvas

--- assets/js/synthaesthesia.js
+++ assets/js/synthaesthesia.js
@@ -7,22 +7,53 @@ $("#fileButton").on("click", function() {
   console.log("Button clicked");
   $("#fileInput").trigger("click");
 });
-$("#fileInput").on("click", function() {
-  console.log("fileInput click triggered");
-});
+
 //when the file input state changes check if it has the right kind of file
 $("#fileInput").on("change", function() {
-  console.log("a file has been chosen!");
   var file = $("#fileInput")[0].files[0];
+  var imageUrl = fileToDataUrl(file);
+  drawImage(imageUrl);
+});
+
+function fileToDataUrl(file) {
   if (file.type.match("image/*")) {
+    //take over the window data url
     window.URL = window.URL || window.webkitURL;
     var imageURL = window.URL.createObjectURL(file);
-
+    return imageURL;
+  } else {
+    alert("File chosen not an image. Please select another file.");
+    return null;
+  }
+}
+function drawImage(url) {
+  // make sure a url is supplied
+  console.log(url);
+  if(url) {
+    //define image object, when it loads, format the canvas and draw
     var image = new Image();
     image.onload = function() {
-      //draw image
-      context.drawImage(image, 0,0);
+      //one dimension will always stay 500
+      var width = 500;
+      var height = 500;
+      //adjust the canvas size based on the picture aspect ratio
+      if (image.width > image.height) {
+        var aspRatio = image.width / image.height;
+        width = aspRatio * 500;
+        canvas.width = width;
+      } else {
+        var aspRatio = image.height / image.width;
+        height = aspRatio * 500;
+        canvas.height = height;
+      }
+      //draw the image to the canvas
+      context.drawImage(image,0,0,width,height)
     };
-    image.src = imageURL;
+    //define the image as the data url
+    image.src = url;
+  } else {
+    // if a url not provided return null
+    return null;
   }
-});
+
+}
